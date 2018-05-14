d4e70857e59a7fd774eff11491babc50c13f1828
Marcus Carlsson <marcuscarlsson@marcuss-macbook.home>
2018-05-14 21:23:32 +0100

    Simple quiz game implemented.

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -1,5 +1,74 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
 
+type problem struct {
+	q string
+	a string
+}
+
+func main() {
+	var (
+		filePath = flag.String("path", "problems.csv", "path to csv file, default to problems.csv")
+		duration = flag.Int("timer", 30, "timer until it's game over. (defaults to 30)")
+	)
+	flag.Parse()
+
+	f, err := os.Open(*filePath)
+	timer := time.NewTimer(time.Second * time.Duration(*duration))
+
+	if err != nil {
+		log.Fatalln("Failed to open file")
+	}
+
+	lines, err := csv.NewReader(f).ReadAll()
+
+	if err != nil {
+		log.Fatalln("Failed to read csv file")
+	}
+
+	score := 0
+
+problemLoop:
+	for _, p := range parseProblems(lines) {
+		answerCh := make(chan string)
+		fmt.Printf("What's %v?", p.q)
+
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			break problemLoop
+		case answer := <-answerCh:
+			if p.a == answer {
+				score++
+				continue problemLoop
+			}
+		}
+
+	}
+
+	fmt.Printf("Your score ended on %v out of %v", score, len(lines))
+}
+
+func parseProblems(lines [][]string) (problems []problem) {
+	for _, l := range lines {
+		problems = append(problems, problem{
+			q: l[0],
+			a: l[1],
+		})
+	}
+
+	return problems
 }
